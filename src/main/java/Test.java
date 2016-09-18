201a5f00bf456e6076f421abd9b70bd0c438197b
chrislee <chrislee1982@163.com>
2016-09-18 23:59:24 +0800

    初次创建项目，框架初步搭建 2016-09-18

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by chrislee on 2016/9/18.
+ */
+public class Test {
+    public static void main(String args[]) {
+        System.out.println("hello world");
+    }
+}
